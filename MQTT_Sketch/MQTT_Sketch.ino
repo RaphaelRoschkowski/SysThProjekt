cbd9f6f390b2aa9619f9213fc0602e76dbc136a0
Blizxzard <flixi.j@outlook.de>
2026-08-07 00:31:51 +0200

    added display test

--- MQTT_Sketch/MQTT_Sketch.ino
+++ MQTT_Sketch/MQTT_Sketch.ino
@@ -1,10 +1,13 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <SPI.h>
+#include <U8g2lib.h>
 
+// AP
 const char* ap_ssid = "ESP32_IOT";
 const char* ap_password = "12345678";
 
-//const uint8_t pi_mac[6] = {0x88, 0xA2, 0x9E, 0xB1, 0x52, 0x74} 
+// MQTT
 const char* mqtt_server = "192.168.4.2"; // Pi's IP once connected to ESP32's AP - check Serial output on Pi or router client list
 const char* mqtt_user = "blueberry";
 const char* mqtt_pass = "blueberry";
@@ -12,11 +15,20 @@ const char* mqtt_pass = "blueberry";
 const char* topic_cmd = "wallbox/relay/cmd";
 const char* topic_status = "wallbox/relay/status";
 
-#define RELAY_PIN 23
+// PINS
+#define RELAY_PIN 12
+#define SPI_DIN 23
+#define SPI_DO 19
+#define SPI_CLK 18
+#define SCREEN_CS 4
+#define SCREEN_DC 17
+#define SCREEN_RES 16
+#define MEAS_CS 5
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+U8G2_SSD1327_WS_128X128_F_4W_HW_SPI u8g2(U8G2_R0, SCREEN_CS, SCREEN_DC, SCREEN_RES);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg;
@@ -51,13 +63,20 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);
 
+  // WIFI
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ap_ssid, ap_password);
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
-  
+  // MQTT
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  // DISPLAY
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_7x14B_tr);
+  u8g2.drawStr(10,20, "SPI_OK");
+  u8g2.sendBuffer();
 }
 
 void loop() {
