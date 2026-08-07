23b065c8ce86d7eecf76832b86722cdcbd77a908
Blizxzard <flixi.j@outlook.de>
2026-08-07 02:17:42 +0200

    Added data topic

--- MQTT_Sketch/MQTT_Sketch.ino
+++ MQTT_Sketch/MQTT_Sketch.ino
@@ -14,6 +14,7 @@ const char* mqtt_pass = "blueberry";
 
 const char* topic_cmd = "wallbox/relay/cmd";
 const char* topic_status = "wallbox/relay/status";
+const char* topic_data = "wallbox/data";
 
 // PINS
 #define RELAY_PIN 12
@@ -28,8 +29,13 @@ const char* topic_status = "wallbox/relay/status";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-U8G2_SSD1327_WS_128X128_F_4W_HW_SPI u8g2(U8G2_R0, SCREEN_CS, SCREEN_DC, SCREEN_RES);
 bool relay_status;
+
+U8G2_SSD1327_WS_128X128_F_4W_HW_SPI u8g2(U8G2_R0, SCREEN_CS, SCREEN_DC, SCREEN_RES);
+SPISettings measSPISettings(1000000, MSBFIRST, SPI_MODE0);
+
+#define DATA_INTERVAL 100
+unsigned long lastDataMillis = 0;
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg;
@@ -38,11 +44,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   if (msg == "ON") {
   //  digitalWrite(RELAY_PIN, LOW);
+    relay_status = true;
     client.publish(topic_status, "relay_on");
     updateDisplay();
 
   } else if (msg == "OFF") {
   //  digitalWrite(RELAY_PIN, HIGH);
+    relay_status = false;
     client.publish(topic_status, "relay_off");
     updateDisplay();
   }
@@ -69,7 +77,20 @@ void updateDisplay() {
   u8g2.drawStr(20,40, client.connected() ? "RPi: Connected" : "RPi: Not Connected");
   u8g2.sendBuffer();
 }
-
+float readMeasurement() {
+  digitalWrite(MEAS_CS, LOW);
+  SPI.beginTransaction(measSPISettings);
+  uint16_t raw = SPI.transfer16(0x0000);
+  SPI.endTransaction();
+  digitalWrite(MEAS_CS, HIGH);
+  return raw * 0.001f; //filler
+}
+void publishMeasurement() {
+  float val = readMeasurement();
+  char payload[64];
+  snprintf(payload, sizeof(payload), "{\"val\":%.3f,\"ts\":%lu}", val, millis());
+  client.publish(topic_data, payload);
+}
 void setup() {
   Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
@@ -92,4 +113,8 @@ void setup() {
 void loop() {
   if (!client.connected()) reconnect();
   client.loop();
+  if (millis() - lastDataMillis >= DATA_INTERVAL) {
+    lastDataMillis = millis();
+    publishMeasurement();
+  }
 }
