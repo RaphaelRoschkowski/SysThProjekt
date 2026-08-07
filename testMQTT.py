a0999fdbb69c350b81df57674b8e6876373c70f2
Blizxzard <flixi.j@outlook.de>
2026-08-07 02:18:32 +0200

    Receive wallbox DAta

--- testMQTT.py
+++ testMQTT.py
@@ -1,13 +1,18 @@
 import paho.mqtt.client as mqtt
+import json
 
 def on_message(client,userdata,msg):
-    print(f"{msg.topic}: {msg.payload.decode()}")
+    if msg.topic == "wallbox/data":
+        data = json.loads(msg.payload.decode())
+        print(f"val={data['val']} ts={data['ts']}")
+    else:
+        print(f"{msg.topic}: {msg.payload.decode()}")
 
 client = mqtt.Client()
 client.username_pw_set("blueberry", "blueberry")
 client.on_message = on_message
 client.connect("192.168.4.2", 1883)
 client.subscribe("wallbox/relay/status")
-
-client.publish("wallbox/relay/cmd", "ON")
+client.subscribe("wallbox/data")
+client.publish("wallbox/relay/cmd", "OFF")
 client.loop_forever()
