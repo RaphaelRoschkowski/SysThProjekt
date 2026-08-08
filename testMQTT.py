5244b8cd0835293db4f51b911fe92b495320de41
Blizxzard <flixi.j@outlook.de>
2026-08-09 00:24:43 +0200

    update Test

--- testMQTT.py
+++ testMQTT.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import json
+import time
 
 def on_message(client,userdata,msg):
     if msg.topic == "wallbox/data":
@@ -7,12 +8,24 @@ def on_message(client,userdata,msg):
         print(f"val={data['val']} ts={data['ts']}")
     else:
         print(f"{msg.topic}: {msg.payload.decode()}")
-
+def on_connect(client, userdata, flags, reason_code, properties):
+    print("Connected; RC:", reason_code)
+    client.subscribe("wallbox/relay/status")
+    client.subscribe("wallbox/data")
+def on_connect_fail(client):
+    print("Could not connect")
+    #start = time.time()
+    #while(time.time()-start <= 3):
+    #    pass
+    #client.connect("192.168.4.2", 1883)
+#setup
 client = mqtt.Client()
 client.username_pw_set("blueberry", "blueberry")
 client.on_message = on_message
+client.on_connect = on_connect
+client.on_connect_fail = on_connect_fail
+
+#code
 client.connect("192.168.4.2", 1883)
-client.subscribe("wallbox/relay/status")
-client.subscribe("wallbox/data")
 client.publish("wallbox/relay/cmd", "OFF")
 client.loop_forever()
